7aac1af0431caefbc86dfa418aedef96c84b82ae
Vladislav Osmanov <7123463+osmanovv@users.noreply.github.com>
2022-06-07 15:40:24 +0300

    ADC to DAC test

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,11 @@
 #include <soc/sens_struct.h>
 #include "driver/adc.h"
 
-#define ADC_SAMPLES_COUNT 1000
-int16_t abuf[ADC_SAMPLES_COUNT];
-int16_t abufPos = 0;
+volatile int16_t adc, dac = 0;
 
 portMUX_TYPE DRAM_ATTR timerMux = portMUX_INITIALIZER_UNLOCKED; 
 TaskHandle_t adcHandlerTask;
 hw_timer_t * adcTimer = NULL; // our timer
-
-void adcHandler(void *param) {
-  while (true) {
-    // Sleep until the ISR gives us something to do, or for 1 second
-    uint32_t tcount = ulTaskNotifyTake(pdFALSE, pdMS_TO_TICKS(1000));  
-    if (true) {
-      // Do something complex and CPU-intensive
-    }
-  }
-}
 
 int IRAM_ATTR local_adc1_read(int channel) {
     uint16_t adc_value;
@@ -40,32 +28,16 @@ int IRAM_ATTR local_adc1_read(int channel) {
 void IRAM_ATTR onTimer() {
   portENTER_CRITICAL_ISR(&timerMux);
 
-  abuf[abufPos++] = local_adc1_read(ADC1_CHANNEL_0);
+  dac = local_adc1_read(ADC1_CHANNEL_0);
   
-  if (abufPos >= ADC_SAMPLES_COUNT) { 
-    abufPos = 0;
+  /* push DAC */
+  dacWrite(DAC1, highByte(dac));
+  dacWrite(DAC2, lowByte(dac)); 
 
-    // Notify adcTask that the buffer is full.
-    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    vTaskNotifyGiveFromISR(adcHandlerTask, &xHigherPriorityTaskWoken);
-    if (xHigherPriorityTaskWoken) {
-      portYIELD_FROM_ISR();
-    }
-  }
   portEXIT_CRITICAL_ISR(&timerMux);
 }
 
 void setup() {
-  //xTaskCreate(adcHandler, "ADC Handler Task", 8192, NULL, 1, &adcHandlerTask);
-  xTaskCreatePinnedToCore(
-      adcHandler, /* Function to implement the task */
-      "ADC Task", /* Name of the task */
-      8192,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      1, /*0*/  /* Priority of the task */
-      &adcHandlerTask,  /* Task handle. */
-      0); /* Core where the task should run */
-
   adcTimer = timerBegin(3, 80, true); // 80 MHz / 80 = 1 MHz hardware clock for easy figuring
   timerAttachInterrupt(adcTimer, &onTimer, true); // Attaches the handler function to the timer 
   timerAlarmWrite(adcTimer, 45, true); // Interrupts when counter == 45, i.e. 22.222 times a second
